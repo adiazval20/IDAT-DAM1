022c1cb59d0c9c65a773d632052df30206ae7d8f
Andy <adiazval20@gmail.com>
2020-08-29 08:19:01 -0500

    0

---
+++ Semana6/app/src/main/java/edu/idat/materialdesign/LoginActivity.java
@@ -0,0 +1,27 @@
+package edu.idat.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Context context = this;
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, ProductosActivity.class));
+            }
+        });
+    }
+}
