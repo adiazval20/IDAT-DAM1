3d7b37efa1c1399459aeddcdfe428a32df25f3ed
Andy <adiazval20@gmail.com>
2020-09-05 08:03:40 -0500

    05/09/2020

---
+++ Semana7/app/src/main/java/edu/idat/materialdesign/PostsActivity.java
@@ -0,0 +1,41 @@
+package edu.idat.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.idat.materialdesign.adapter.PostAdapter;
+import edu.idat.materialdesign.entity.Post;
+
+public class PostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Lista de Publicaciones");
+
+        ListView lsvPosts = findViewById(R.id.lsvPosts);
+//        List<String> list = new ArrayList<>();
+//        list.add("Elemento 1");
+//        list.add("Elemento 2");
+//        list.add("Elemento 3");
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1, "Iphone", "29/08/2020", R.drawable.iphone, "Esta es una publicación", R.drawable.chat));
+        posts.add(new Post(2, "Samsung", "28/08/2020", R.drawable.samsung2, "Esta es otra publicación", R.drawable.logo));
+
+        PostAdapter adapter = new PostAdapter(this, R.layout.item_post, posts);
+
+        lsvPosts.setAdapter(adapter);
+    }
+}
