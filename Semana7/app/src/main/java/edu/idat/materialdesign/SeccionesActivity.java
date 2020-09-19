42017d23fe14e9f5bc25a5fb519d19f03e004ca3
Andy <adiazval20@gmail.com>
2020-09-19 08:07:20 -0500

    18/09/2020

--- Semana7/app/src/main/java/edu/idat/materialdesign/SeccionesActivity.java
+++ Semana7/app/src/main/java/edu/idat/materialdesign/SeccionesActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.idat.materialdesign.adapter.SeccionAdapter;
+import edu.idat.materialdesign.entity.Seccion;
 
 public class SeccionesActivity extends AppCompatActivity {
 
@@ -20,14 +24,13 @@ public class SeccionesActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Secciones");
 
-        List<String> lstSecciones = new ArrayList<>();
-        lstSecciones.add("Inicio");
-        lstSecciones.add("Productos");
-        lstSecciones.add("Artículos");
-        lstSecciones.add("Ayuda");
+        GridView grvSecciones = findViewById(R.id.grvSecciones);
 
-        ListView lsvSecciones = findViewById(R.id.lsvSecciones);
-        ArrayAdapter<String> seccionesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lstSecciones);
-        lsvSecciones.setAdapter(seccionesAdapter);
+        List<Seccion> lstSecciones = new ArrayList<>();
+        lstSecciones.add(new Seccion(1, "Inicio", R.drawable.ic_baseline_home_24, "#aab6fe", "#001064"));
+        lstSecciones.add(new Seccion(2, "Tienda", R.drawable.ic_baseline_shopping_cart_24, "#ffffa8", "#f57f17"));
+
+        SeccionAdapter seccionAdapter = new SeccionAdapter(this, R.layout.item_seccion, lstSecciones);
+        grvSecciones.setAdapter(seccionAdapter);
     }
 }
--- Semana7/app/src/main/java/edu/idat/materialdesign/adapter/SeccionAdapter.java
+++ Semana7/app/src/main/java/edu/idat/materialdesign/adapter/SeccionAdapter.java
@@ -1,15 +1,21 @@
 package edu.idat.materialdesign.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.List;
 
+import edu.idat.materialdesign.R;
 import edu.idat.materialdesign.entity.Seccion;
 
 public class SeccionAdapter extends ArrayAdapter<Seccion> {
@@ -22,6 +28,22 @@ public class SeccionAdapter extends ArrayAdapter<Seccion> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Seccion seccion = getItem(position);
 
-        return super.getView(position, convertView, parent);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_seccion, parent, false);
+        }
+
+        LinearLayout lytSeccion = convertView.findViewById(R.id.lytSeccion);
+        ImageView imgIcono = convertView.findViewById(R.id.imgIcono);
+        TextView txtNombre = convertView.findViewById(R.id.txtNombre);
+
+        lytSeccion.setBackgroundColor(Color.parseColor(seccion.getHexBackColor()));
+
+        imgIcono.setImageResource(seccion.getImagenId());
+        imgIcono.setColorFilter(Color.parseColor(seccion.getHexColor()));
+
+        txtNombre.setText(seccion.getNombre());
+        txtNombre.setTextColor(Color.parseColor(seccion.getHexColor()));
+
+        return convertView;
     }
 }
