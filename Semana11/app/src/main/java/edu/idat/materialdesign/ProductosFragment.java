72bd319cbd34bfa33431e81e17fc01551d5f99bc
Andy <adiazval20@gmail.com>
2020-10-03 11:02:24 -0500

    2

--- Semana11/app/src/main/java/edu/idat/materialdesign/ProductosFragment.java
+++ Semana11/app/src/main/java/edu/idat/materialdesign/ProductosFragment.java
@@ -12,11 +12,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.idat.materialdesign.adapter.CategoriaAdapter;
+import edu.idat.materialdesign.adapter.CategoriaSpinnerAdapter;
 import edu.idat.materialdesign.adapter.ProductoAdapter;
+import edu.idat.materialdesign.entity.Categoria;
 import edu.idat.materialdesign.entity.Producto;
 
 public class ProductosFragment extends Fragment implements IComunicacion {
@@ -33,6 +40,36 @@ public class ProductosFragment extends Fragment implements IComunicacion {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Cargar lista de categorías
+        RecyclerView rcvCategorias = view.findViewById(R.id.rcvCategorias);
+        rcvCategorias.setLayoutManager(new GridLayoutManager(getContext(), 1, RecyclerView.HORIZONTAL, false));
+
+        final List<Categoria> categorias = new ArrayList<>();
+        categorias.add(new Categoria(1, "Celulares", R.drawable.chat));
+        categorias.add(new Categoria(2, "Vehículos", R.drawable.coche));
+
+        CategoriaAdapter categoriaAdapter = new CategoriaAdapter(categorias);
+        rcvCategorias.setAdapter(categoriaAdapter);
+
+        // Cargar lista de categorias en Spinner
+        Spinner spCategorias = view.findViewById(R.id.spCategorias);
+
+        CategoriaSpinnerAdapter spinnerAdapter = new CategoriaSpinnerAdapter(getContext(), R.layout.item_categoria_spinner, categorias);
+        spCategorias.setAdapter(spinnerAdapter);
+
+        spCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), categorias.get(position).getNombre(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // Cargar lista de productos
         RecyclerView rcvProductos = view.findViewById(R.id.rcvProductos);
         rcvProductos.setLayoutManager(new GridLayoutManager(getContext(), 1));
 
